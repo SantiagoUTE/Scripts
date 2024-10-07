6f29bdd4f6610f38bc4e88dc1dbaa546d554aec1
SantiagoUTE <sflor@ute.edu.ec>
2024-10-07 16:11:39 -0500

    Create Autocad.bat

---
+++ Autocad.bat
@@ -0,0 +1,1 @@
+"\\UIODTITEC001\Users\sflor\Desktop\seb\image\Installer.exe" -i deploy --offline_mode -q -o "\\UIODTITEC001\Users\sflor\Desktop\seb\image\Collection.xml" --installer_version "2.9.0.31"
