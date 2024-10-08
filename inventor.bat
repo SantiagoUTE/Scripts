13f97c3e31f4df02f0f7dbf01edd622b69c830e9
SantiagoUTE <sflor@ute.edu.ec>
2024-10-08 12:10:56 -0500

    inventor.bat

--- inventor.bat
+++ inventor.bat
@@ -1,1 +1,1 @@
-\\svrquito201\Matlab2016a\Inventor2025\image\Installer.exe" -i deploy --offline_mode -q -o "\\svrquito201\Matlab2016a\Inventor2025\image\Collection.xml" --installer_version "2.9.0.31"
+"\\svrquito201\Matlab2016a\Inventor2025\image\Installer.exe" -i deploy --offline_mode -q -o "\\svrquito201\Matlab2016a\Inventor2025\image\Collection.xml" --installer_version "2.9.0.31"
