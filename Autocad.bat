1909fe0cf390b66b7a69d2c2cf7458895e152cbf
SantiagoUTE <sflor@ute.edu.ec>
2024-10-08 10:23:52 -0500

    Autocad.bat

--- Autocad.bat
+++ Autocad.bat
@@ -1,1 +1,1 @@
-"\\UIODTITEC001\Users\sflor\Desktop\seb\image\Installer.exe" -i deploy --offline_mode -q -o "\\UIODTITEC001\Users\sflor\Desktop\seb\image\Collection.xml" --installer_version "2.9.0.31"
+"\\svrquito201\Matlab2016a\Atocad2025\image\Installer.exe" -i deploy --offline_mode -q -o "\\svrquito201\Matlab2016a\Atocad2025\image\Collection.xml" --installer_version "2.9.0.31"
