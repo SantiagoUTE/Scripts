8c5e204b6ef90057d23ef23babc4764f3174dafc
SantiagoUTE <sflor@ute.edu.ec>
2026-02-13 11:45:31 -0500

    Update copiarR.bat

--- copiarR.bat
+++ copiarR.bat
@@ -1,1 +1,19 @@
+@echo off
+echo ============================================
+echo Copiando archivos desde \\SVRPROGRAMAS\Compartido
+echo hacia C:\Compartido
+echo ============================================
+
+REM Crear carpeta destino si no existe
+if not exist "C:\Compartido" (
+    mkdir "C:\Compartido"
+)
+
+REM Ejecutar Robocopy
 robocopy "\\SVRPROGRAMAS\Compartido" "C:\Compartido" /E /COPY:DAT /R:2 /W:5
+
+echo ============================================
+echo Copia completada.
+echo ============================================
+
+pause
